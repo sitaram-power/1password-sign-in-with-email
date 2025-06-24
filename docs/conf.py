fd64005985dd27a853f36485d9547e96f327c87c
sitaram-power <chemmililli@hotmail.com>
2025-06-24 12:32:12 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+project = '1password Account'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+html_js_files = [
+    'chat.js',
+    '_static/chat.js',
+]
